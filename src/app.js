c82e87d9500ceab76e1ac16c0f46489352ef9fc8
msrijai <j@js-MacBook-Pro.local>
2019-12-22 23:03:57 -0500

    remove channels

--- src/app.js
+++ src/app.js
@@ -8,13 +8,11 @@ const logger = require('./logger');
 const feathers = require('@feathersjs/feathers');
 const configuration = require('@feathersjs/configuration');
 const express = require('@feathersjs/express');
-const socketio = require('@feathersjs/socketio');
 
 
 const middleware = require('./middleware');
 const services = require('./services');
 const appHooks = require('./app.hooks');
-const channels = require('./channels');
 
 const mongodb = require('./mongodb');
 
@@ -34,7 +32,6 @@ app.use('/', express.static(app.get('public')));
 
 // Set up Plugins and providers
 app.configure(express.rest());
-// app.configure(socketio());
 
 app.configure(mongodb);
 
@@ -42,8 +39,6 @@ app.configure(mongodb);
 app.configure(middleware);
 // Set up our services (see `services/index.js`)
 app.configure(services);
-// Set up event channels (see channels.js)
-app.configure(channels);
 
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
