333ecf16be73207de02dd28c6fee2ad676e2ee3a
msrijai <j@j-MacBook-Pro.local>
2019-12-31 16:15:52 -0500

    Discord webhook URL

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-console */
+const dotenv = require('dotenv');
+dotenv.config();
+
 const logger = require('./logger');
 const app = require('./app');
 const hostname = app.get('host');
