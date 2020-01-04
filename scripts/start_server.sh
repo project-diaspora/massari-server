74f6efe6077e97b1e4c229a654cf1f27a056ac43
Joseph Haddad <joseph.haddad789@gmail.com>
2020-01-04 14:53:43 -0500

    Testing deployment v7

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,5 +4,5 @@
 #          Start the node server
 # ------------------------------------------------------------------
 forever stopall
-cd /var/www/html/massari-server/src
-DISCORD_WEBHOOK_URL=$DISCORD_WEBHOOK_URL forever start index.js
+cd /var/www/html/massari-server
+DISCORD_WEBHOOK_URL=$DISCORD_WEBHOOK_URL forever start ./forever/development.json
