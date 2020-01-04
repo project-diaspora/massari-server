48881684d5164875d7bb1f1f965259812f786910
Joseph Haddad <joseph.haddad789@gmail.com>
2020-01-03 21:59:15 -0500

    Adding DISCORD URL

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,7 @@
 # [Author] Joseph Haddad
 #          Start the node server
 # ------------------------------------------------------------------
-cd /usr/src/massari-server && sudo npm install forever --global
+cd /usr/src/massari-server
+sudo npm install forever --global
 forever stopall
 DISCORD_WEBHOOK_URL=$DISCORD_WEBHOOK_URL forever start src/index.js
