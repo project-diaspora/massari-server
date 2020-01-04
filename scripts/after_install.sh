677fb8fe088c74b5820d5b59cd835fdf68c3f6a7
Joseph Haddad <joseph.haddad789@gmail.com>
2020-01-03 22:32:30 -0500

    Adding DISCORD URL

---
+++ scripts/after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# [Author] Joseph Haddad
+#          Update local debian packages
+# ------------------------------------------------------------------
+chmod +x /usr/src/massari-server/scripts/start_server.sh
