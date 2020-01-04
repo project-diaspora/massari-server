525f560c684d39c217ffaef7f1028dcb53e559d2
msrijai <58372275+msrijai@users.noreply.github.com>
2020-01-04 10:10:48 -0500

    fix logger (#10)

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,10 @@
 const DiscordTransport = require('winston-discord-transport').default;
 const { createLogger, format, transports } = require('winston');
-const app = require('./app');
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
-  level: 'info',
+  level: 'debug',
   format: format.combine(
     format.splat(),
     format.simple()
@@ -17,9 +16,8 @@ const logger = createLogger({
 
 if (!['test', 'dev'].includes(process.env.NODE_ENV)) {
   logger.add(new DiscordTransport({
-    webhook: app.get('discordWebhookUrl'),
-  })
-  );
+    webhook: process.env.DISCORD_WEBHOOK_URL,
+  }));
 }
 
 module.exports = logger;
