3a5355fae3e057437c8be8f2d0ae93051dde81c7
Joseph Haddad <joseph.haddad789@gmail.com>
2020-01-04 13:49:17 -0500

    Testing deployment v1

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,6 @@
 # [Author] Joseph Haddad
 #          Start the node server
 # ------------------------------------------------------------------
-sudo npm install pm2 --global
-pm2 stop all
-DISCORD_WEBHOOK_URL=$DISCORD_WEBHOOK_URL pm2 start /var/www/html/massari-server/src/index.js
+cd /usr/src/massari-server && sudo npm install forever --global
+forever stopall
+DISCORD_WEBHOOK_URL=$DISCORD_WEBHOOK_URL forever start src/index.js
