8f0ffb5598bc86fc398c0dd77ff4314c23402f90
msrijai <j@mbp.local>
2020-01-04 20:57:39 -0400

    fix server port

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ dotenv.config();
 
 const logger = require('./logger');
 const app = require('./app');
-const hostname = app.get('host');
-const port = app.get('port');
-const server = app.listen(process.env.PORT || port, hostname);
+const port = process.env.PORT || app.get('port');
+const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
 server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', hostname, port)
+  logger.info('Feathers application started on port %s', port)
 );
